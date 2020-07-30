0589b5be1343bd911b64c6de39479b1e57b4b25e
KhiemLuc <41050460+luckhiem@users.noreply.github.com>
2020-07-30 10:57:53 +0700

    update

--- src/main/Component/GenerateForm/GenerateConfigForm.jsx
+++ src/main/Component/GenerateForm/GenerateConfigForm.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import { ProfileContext } from '../../Layout/app';
 import 'antd/dist/antd.css';
 import './index.css';
 import { Form, Row, Col, Switch, Button, Input, InputNumber, Checkbox } from 'antd';
@@ -44,11 +45,22 @@ const CheckboxApp = () => {
 }
 
 const GenerateConfigForm = () => {
+    const { profiles, setProfiles } = useContext(ProfileContext);
     const [isVisibleApp, setVisibleApp] = useState('none');
     const [isVisibleSpace, setVisibleSpace] = useState('none');
     const [switchAppChecked, setSwitchAppChecked] = useState(false);
     const [switchSpaceChecked, setSwitchSpaceChecked] = useState(false)
     const [form] = Form.useForm();
+    const GenerateConfig = {
+        generateGroup: true,
+        generateUser: true,
+        generateOrg: true,
+        app_name: '',
+        app_numbers: '',
+        app_types: '',
+        space_name: '',
+        space_numbers: ''
+    }
 
     const formItemLayout = {
         labelCol: {
@@ -69,6 +81,7 @@ const GenerateConfigForm = () => {
             {...formItemLayout}
             name="advanced_search"
             className="ant-advanced-search-form"
+            initialValues={GenerateConfig}
             onFinish={onFinish}
         >
             <Row>
@@ -96,13 +109,46 @@ const GenerateConfigForm = () => {
                                 }
                             }} />
                     </Form.Item>
-                    <Form.Item label='App Name' style={{ display: isVisibleApp }}>
+                    <Form.Item label='App Name' name="app_name" style={{ display: isVisibleApp }}>
                         <Input placeholder="Input App Name" />
                     </Form.Item>
-                    <Form.Item label='Select App Generate' style={{ display: isVisibleApp }}>
-                        <CheckboxApp />
+                    <Form.Item label='Select App Generate' name="app_types" style={{ display: isVisibleApp }}>
+                        <Checkbox.Group style={{ width: '100%' }}>
+                            <Row>
+                                <Col span={12}>
+                                    <Checkbox value="appInGuestSpace">App In Guest Space</Checkbox>
+                                </Col>
+                                <Col span={12}>
+                                    <Checkbox value="appInSpace">App In Space</Checkbox>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col span={12}>
+                                    <Checkbox value="appWithMultipleFields">App With Multiple Fields</Checkbox>
+                                </Col>
+                                <Col span={12}>
+                                    <Checkbox value="appWithRequiredFields">App With Required Fields</Checkbox>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col span={12}>
+                                    <Checkbox value="appWithUniqueFields">App With Unique Fields</Checkbox>
+                                </Col>
+                                <Col span={12}>
+                                    <Checkbox value="appWithoutField">App Without Field</Checkbox>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col span={12}>
+                                    <Checkbox value="appWithThoroughFields">App With Thorough Fields</Checkbox>
+                                </Col>
+                                <Col span={12}>
+                                    <Checkbox value="appWithProcess">App With Process</Checkbox>
+                                </Col>
+                            </Row>
+                        </Checkbox.Group>
                     </Form.Item>
-                    <Form.Item label='Number of Apps' style={{ display: isVisibleApp }}>
+                    <Form.Item label='Number of Apps' name="app_numbers" style={{ display: isVisibleApp }}>
                         <InputNumber />
                     </Form.Item>
                     <Form.Item label='Generate space'>
@@ -119,18 +165,26 @@ const GenerateConfigForm = () => {
                                 }
                             }} />
                     </Form.Item>
-                    <Form.Item label='Space Name' style={{ display: isVisibleSpace }}>
+                    <Form.Item label='Space Name' name="space_name" style={{ display: isVisibleSpace }}>
                         <Input placeholder="Input Space Name" />
                     </Form.Item>
-                    <Form.Item label='Number of spaces' style={{ display: isVisibleSpace }}>
+                    <Form.Item label='Number of spaces' name="space_numbers" style={{ display: isVisibleSpace }}>
                         <InputNumber />
                     </Form.Item>
                 </Col>
             </Row>
             <Row>
                 <Col span={24} style={{ textAlign: 'right' }}>
-                    <Button type="primary" htmlType="submit">Save</Button>
-                    <Button style={{ margin: '0 8px' }} onClick={() => { form.resetFields() }}>Edit</Button>
+                    <Button type="primary" htmlType="submit" onClick={() => {
+                        form.validateFields()
+                            .then((values) => {
+                                console.log('value', values)
+                            })
+                            .catch((error) => {
+                                console.log(JSON.stringify(error))
+                            })
+                    }}>Save</Button>
+                    <Button style={{ margin: '0 8px' }}>Edit</Button>
                 </Col>
             </Row>
         </Form>
