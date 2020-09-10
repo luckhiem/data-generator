a25906d4659a1d605c6a68b2af420bee382ee427
Luc Tran Vinh Khiem <k001310@PC-001310-4.local>
2020-09-10 14:20:59 +0700

    update

--- src/main/Component/GenerateForm/GenerateConfigForm.jsx
+++ src/main/Component/GenerateForm/GenerateConfigForm.jsx
@@ -2,7 +2,7 @@ import React, { useState, useContext, useEffect } from 'react';
 import { ProfileContext } from '../../Layout/app';
 import 'antd/dist/antd.css';
 import './index.css';
-import { Form, Row, Col, Switch, Button, Input, InputNumber, Checkbox } from 'antd';
+import { Form, Row, Col, Switch, Button, Input, InputNumber, Checkbox, Divider } from 'antd';
 import AppConfigTable from './AppTableConfig.jsx';
 import SpaceConfigTable from './SpaceTableConfig';
 
@@ -11,7 +11,7 @@ const GenerateConfigForm = (profileIdCopy) => {
     const [isDisable, setDisable] = useState(true);
     const [isVisibleSaveBtn, setVisibleSaveBtn] = useState('none');
     const [isVisibleApp, setVisibleApp] = useState('flex');
-    const [isVisibleSpace, setVisibleSpace] = useState('none');
+    const [isVisibleSpace, setVisibleSpace] = useState('flex');
     const [form] = Form.useForm();
     let GenerateConfig = {
         generateGroup: true,
@@ -62,66 +62,68 @@ const GenerateConfigForm = (profileIdCopy) => {
             className="ant-advanced-search-form"
             initialValues={GenerateConfig}
         >
-            <Row>
-                <Col span={16}>
-                    <Form.Item label='Generate organization' name="generateOrg" valuePropName="checked">
-                        <Switch checkedChildren="YES" unCheckedChildren="NO" defaultChecked disabled={true} />
-                    </Form.Item>
-                    <Form.Item label='Generate group' name="generateGroup" valuePropName="checked">
-                        <Switch checkedChildren="YES" unCheckedChildren="NO" defaultChecked disabled={true} />
-                    </Form.Item>
-                    <Form.Item label='Generate users' name="generateUser" valuePropName="checked">
-                        <Switch checkedChildren="YES" unCheckedChildren="NO" defaultChecked disabled={true} />
-                    </Form.Item>
-                    <Form.Item label='Generate app' name="generateApp" valuePropName="checked">
-                        <Switch
-                            checkedChildren="YES"
-                            unCheckedChildren="NO"
-                            onChange={(value) => {
-                                if (value === true) {
-                                    setVisibleApp('flex')
-                                } else {
-                                    setVisibleApp('none')
-                                }
-                            }}
-                            disabled={isDisable}
-                        />
-                    </Form.Item>
-                </Col>
+            <div disabled={isDisable}>
+                <Row>
+                    <Col span={20}>
+                        <Form.Item label='Generate organization' name="generateOrg" valuePropName="checked">
+                            <Switch checkedChildren="YES" unCheckedChildren="NO" defaultChecked disabled={true} />
+                        </Form.Item>
+                        <Form.Item label='Generate group' name="generateGroup" valuePropName="checked">
+                            <Switch checkedChildren="YES" unCheckedChildren="NO" defaultChecked disabled={true} />
+                        </Form.Item>
+                        <Form.Item label='Generate users' name="generateUser" valuePropName="checked">
+                            <Switch checkedChildren="YES" unCheckedChildren="NO" defaultChecked disabled={true} />
+                        </Form.Item>
+                        <Form.Item label='Generate app' name="generateApp" valuePropName="checked">
+                            <Switch
+                                checkedChildren="YES"
+                                unCheckedChildren="NO"
+                                onChange={(value) => {
+                                    if (value === true) {
+                                        setVisibleApp('flex')
+                                    } else {
+                                        setVisibleApp('none')
+                                    }
+                                }}
+                                defaultChecked
+                            />
+                        </Form.Item>
+                    </Col>
 
-            </Row>
-            <Row>
-                <Col span={16}>
-                    <Form.Item label='Config App' style={{ display: isVisibleApp }}>
-                        <AppConfigTable profileId={profileIdCopy.profileId} />
-                    </Form.Item>
-                </Col>
-            </Row>
-            <Row>
-                <Col span={16}>
-                    <Form.Item label='Generate space' name="generateSpace" valuePropName="checked">
-                        <Switch
-                            checkedChildren="YES"
-                            unCheckedChildren="NO"
-                            onChange={(value) => {
-                                if (value === true) {
-                                    setVisibleSpace('flex')
-                                } else {
-                                    setVisibleSpace('none')
-                                }
-                            }}
-                            disabled={isDisable}
-                        />
-                    </Form.Item>
-                </Col>
-            </Row>
-            <Row>
-                <Col span={16}>
-                    <Form.Item label='Config Space' style={{ display: isVisibleApp }}>
-                        <SpaceConfigTable profileId={profileIdCopy.profileId} />
-                    </Form.Item>
-                </Col>
-            </Row>
+                </Row>
+                <Row>
+                    <Col span={20}>
+                        <Form.Item label='Config App' style={{ display: isVisibleApp }}>
+                            <AppConfigTable profileId={profileIdCopy.profileId} />
+                        </Form.Item>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col span={20}>
+                        <Form.Item label='Generate space' name="generateSpace" valuePropName="checked">
+                            <Switch
+                                checkedChildren="YES"
+                                unCheckedChildren="NO"
+                                onChange={(value) => {
+                                    if (value === true) {
+                                        setVisibleSpace('flex')
+                                    } else {
+                                        setVisibleSpace('none')
+                                    }
+                                }}
+                                defaultChecked
+                            />
+                        </Form.Item>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col span={20}>
+                        <Form.Item label='Config Space' style={{ display: isVisibleSpace }}>
+                            <SpaceConfigTable profileId={profileIdCopy.profileId} />
+                        </Form.Item>
+                    </Col>
+                </Row>
+            </div>
             <Row>
                 <Col span={24} style={{ textAlign: 'right' }}>
                     <Button
@@ -146,7 +148,7 @@ const GenerateConfigForm = (profileIdCopy) => {
                                     console.log(error)
                                 })
                         }}>Save</Button>
-                    <Button style={{ margin: '0 8px' }} onClick={() => {
+                    <Button style={{ margin: '0 8px', zIndex: '1000' }} onClick={() => {
                         setVisibleSaveBtn('inline')
                         setDisable(false)
                     }}>Edit</Button>
