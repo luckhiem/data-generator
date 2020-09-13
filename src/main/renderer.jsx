d8786d9cbd7f10f8cb2aca28bfa66cd3384bf794
Luc Tran Vinh Khiem <k001310@PC-001310-4.local>
2020-09-13 12:04:06 +0700

    refactor

--- src/main/renderer.jsx
+++ src/main/renderer.jsx
@@ -1,11 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './Layout/app'
-// import * as ServiceWorker from '../serviceWorker'
 
 ReactDOM.render(
   <App />,
   document.getElementById('app')
 )
-
-// ServiceWorker.register();
