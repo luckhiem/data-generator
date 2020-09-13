404f5c475f3cb2155189cf7833b3f4d78e66ef13
Luc Tran Vinh Khiem <k001310@PC-001310-4.local>
2020-09-13 10:42:52 +0700

    update

--- webpack.rules.js
+++ webpack.rules.js
@@ -9,16 +9,6 @@ module.exports = [
     test: /\.node$/,
     use: 'node-loader',
   },
-  // {
-  //   test: /\.(m?js|node)$/,
-  //   parser: { amd: false },
-  //   use: {
-  //     loader: '@marshallofsound/webpack-asset-relocator-loader',
-  //     options: {
-  //       outputAssetBase: 'native_modules',
-  //     },
-  //   },
-  // },
   {
     test: /\.js$/,
     loader: 'babel-loader',
