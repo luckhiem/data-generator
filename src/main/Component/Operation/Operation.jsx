9b4551c32423b9d0f21e6bcc5e1cb01ed41f68e0
KhiemLuc <41050460+luckhiem@users.noreply.github.com>
2020-09-14 14:28:10 +0700

    update

--- src/main/Component/Operation/Operation.jsx
+++ src/main/Component/Operation/Operation.jsx
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import 'antd/dist/antd.css';
+import { Timeline, Row, Col } from 'antd';
 
 const Operation = () => {
+    const [mode, setMode] = useState('right');
+
     return (
-        <div>
-            This is Operation Page
-        </div>
+        <Timeline mode={mode}>
+            <Timeline.Item label="2015-09-01">Create a services</Timeline.Item>
+            <Timeline.Item label="2015-09-01 09:12:11">Solve initial network problems</Timeline.Item>
+            <Timeline.Item label="2015-09-01">Technical testing</Timeline.Item>
+            <Timeline.Item label="2015-09-01 09:12:11">Network problems being solved</Timeline.Item>
+            <Timeline.Item label="2015-09-01">Create a services</Timeline.Item>
+            <Timeline.Item label="2015-09-01 09:12:11">Solve initial network problems</Timeline.Item>
+            <Timeline.Item label="2015-09-01">Technical testing</Timeline.Item>
+            <Timeline.Item label="2015-09-01 09:12:11">Network problems being solved</Timeline.Item>
+            <Timeline.Item label="2015-09-01">Create a services</Timeline.Item>
+            <Timeline.Item label="2015-09-01 09:12:11">Solve initial network problems</Timeline.Item>
+            <Timeline.Item label="2015-09-01">Technical testing</Timeline.Item>
+            <Timeline.Item label="2015-09-01 09:12:11">Network problems being solved</Timeline.Item>
+            <Timeline.Item label="2015-09-01">Create a services</Timeline.Item>
+            <Timeline.Item label="2015-09-01 09:12:11">Solve initial network problems</Timeline.Item>
+            <Timeline.Item label="2015-09-01">Technical testing</Timeline.Item>
+        </Timeline>
     )
 }
 export default Operation;
