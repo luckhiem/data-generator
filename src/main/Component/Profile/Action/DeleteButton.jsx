5c1b1d71609b213e53d1a81eb2529ec75474c615
Luc Tran Vinh Khiem <k001310@PC-001310-4.local>
2020-09-15 10:32:38 +0700

    update

--- src/main/Component/Profile/Action/DeleteButton.jsx
+++ src/main/Component/Profile/Action/DeleteButton.jsx
@@ -1,14 +1,12 @@
 import React, { useState, useContext } from 'react';
 import { Button, Modal } from 'antd';
 import { ProfileContext } from '../../../Layout/app';
-import { Link } from 'react-router-dom';
 
 const DeleteButton = (profileId) => {
     const { profiles, setProfiles } = useContext(ProfileContext);
     const [deleteModalVisible, setDeleteModalVisible] = useState(true);
     const CONFIRM_DELETE_WARNING = "Are you sure to delete this profile ? This action CAN NOT be reverted.";
     const hash = window.location.hash.toString();
-    console.log(hash)
 
     const removeProfile = (profileId) => {
         let profilesRemain = [...profiles]
@@ -18,32 +16,30 @@ const DeleteButton = (profileId) => {
     };
 
     return (
-        <>
-            <Button
-                key="1"
-                type="primary"
-                danger
-                onClick={() => {
-                    Modal.confirm({
-                        title: 'Delete Profile',
-                        content: CONFIRM_DELETE_WARNING,
-                        isVisible: deleteModalVisible,
-                        onOk() {
-                            if (hash === `#/`) {
-                                removeProfile(profileId.profileId);
-                            } else{
-                                window.history.back()
-                                removeProfile(profileId.profileId);
-                            }
-                        },
-                        onCancel() {
-                            setDeleteModalVisible(false);
-                        },
-                    })
-                }}>
-                Delete
+        <Button
+            key="1"
+            type="primary"
+            danger
+            onClick={() => {
+                Modal.confirm({
+                    title: 'Delete Profile',
+                    content: CONFIRM_DELETE_WARNING,
+                    isVisible: deleteModalVisible,
+                    onOk() {
+                        if (hash === `#/`) {
+                            removeProfile(profileId.profileId);
+                        } else {
+                            window.history.back()
+                            removeProfile(profileId.profileId);
+                        }
+                    },
+                    onCancel() {
+                        setDeleteModalVisible(false);
+                    },
+                })
+            }}>
+            Delete
         </Button>
-        </>
     )
 }
 
