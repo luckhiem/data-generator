551dce20389cc051e5cbd8e89e869bcdffde8287
Luc Tran Vinh Khiem <k001310@PC-001310-4.local>
2020-09-15 10:31:35 +0700

    update

--- src/main/Component/Operation/Operation.jsx
+++ src/main/Component/Operation/Operation.jsx
@@ -1,29 +1,46 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState, createContext } from 'react';
 import 'antd/dist/antd.css';
-import { Timeline, Row, Col } from 'antd';
+import { Table } from 'antd';
 
 const Operation = () => {
     const [mode, setMode] = useState('right');
 
+    const columns = [
+        {
+            title: 'Time',
+            dataIndex: 'time',
+            key: 'time',
+        },
+        {
+            title: 'Profile ID',
+            dataIndex: 'PID',
+            key: 'PID',
+            render: text => <a>{text}</a>,
+        },
+        {
+            title: 'Profile Name',
+            dataIndex: 'profileName',
+            key: 'profileName',
+        },
+        {
+            title: 'Operation',
+            dataIndex: 'operation',
+            key: 'operation',
+        },
+    ];
+
+    const data = [
+        {
+            key: '1',
+            time: '29-03-1995',
+            PID: 'PID-123456',
+            profileName: 'John Brown',
+            operation: 'Generate data',
+        }
+    ];
+
     return (
-        <Timeline mode={mode}>
-            <Timeline.Item label="2015-09-01">Create a services</Timeline.Item>
-            <Timeline.Item label="2015-09-01 09:12:11">Solve initial network problems</Timeline.Item>
-            <Timeline.Item label="2015-09-01">Technical testing</Timeline.Item>
-            <Timeline.Item label="2015-09-01 09:12:11">Network problems being solved</Timeline.Item>
-            <Timeline.Item label="2015-09-01">Create a services</Timeline.Item>
-            <Timeline.Item label="2015-09-01 09:12:11">Solve initial network problems</Timeline.Item>
-            <Timeline.Item label="2015-09-01">Technical testing</Timeline.Item>
-            <Timeline.Item label="2015-09-01 09:12:11">Network problems being solved</Timeline.Item>
-            <Timeline.Item label="2015-09-01">Create a services</Timeline.Item>
-            <Timeline.Item label="2015-09-01 09:12:11">Solve initial network problems</Timeline.Item>
-            <Timeline.Item label="2015-09-01">Technical testing</Timeline.Item>
-            <Timeline.Item label="2015-09-01 09:12:11">Network problems being solved</Timeline.Item>
-            <Timeline.Item label="2015-09-01">Create a services</Timeline.Item>
-            <Timeline.Item label="2015-09-01 09:12:11">Solve initial network problems</Timeline.Item>
-            <Timeline.Item label="2015-09-01">Technical testing</Timeline.Item>
-        </Timeline>
+        <Table columns={columns} dataSource={data} />
     )
 }
 export default Operation;
